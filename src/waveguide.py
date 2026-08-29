10645d3109565ef64062f4a7bc03dce316ef5f79
aks44l <aksalmanoj11@gmail.com>
2026-08-29 19:50:08 +0530

    added wavelength, k, k0, k_squared

--- src/waveguide.py
+++ src/waveguide.py
@@ -31,14 +31,26 @@ def create_waveguide_profile(x, width, n_core, n_clad):
 n_core = 3.48  
 n_clad = 1.44
 
+# wavelength
+wavelength = 1310e-9
+
+# free space number
+# ko free space wavenumber ko = 2 * 3.14 / wavelength
+k0 =  2* np.pi / wavelength
+
 # Number of grid points
-N_values = [50, 100, 500, 1000, 5000]
+# N_values = [50, 100, 500, 1000, 5000]
+
+# grid
+N = 1000
 
 
 # simulation region
 # x = np.linspace(-2e-6, 2e-6,1000 )
 x_min = -2e-6
 x_max = 2e-6
+
+x = np.linspace(x_min, x_max, N)
 
 # different waveguide widths
 # widths =[
@@ -50,26 +62,73 @@ x_max = 2e-6
 # single width
 width = 450e-9
 
-# generate and plot each profile
-for N in N_values:
-
-    # grid with N points
-    x = np.linspace(x_min, x_max, N)
-
-    # refractive index profile
-    n = create_waveguide_profile(
+n = create_waveguide_profile(
         x,
         width,
         n_core,
         n_clad
     )
 
-    # plot
-    plt.plot(
-        x * 1e6,
-        n,
-        label=f"N = {N}"
-    )
+# generate and plot each profile
+# for N in N_values:
+
+#     # grid with N points
+#     # x = np.linspace(x_min, x_max, N)
+
+#     # refractive index profile
+#     n = create_waveguide_profile(
+#         x,
+#         width,
+#         n_core,
+#         n_clad
+#     )
+
+    # material-dependent wavenumber
+k = k0 * n
+
+# k**2
+k_squared = k0**2 * n**2
+
+
+# quantities
+print("Wavelength: ", wavelength, "m")
+print("Wavelength: ", wavelength * 1e9 , "nm")
+
+
+print("k0: ", k0,"1/m" )
+
+print("Core wavenumber :", k0* n_core, "1/m")
+print("Cladding wavenumber: ", k0* n_clad,"1/m")
+
+print()
+print("Sanity check:")
+print("n_core =", n_core)
+print("n_clad =", n_clad)
+print("k_core / k0 =", (k0 * n_core) / k0)
+print("k_clad / k0 =", (k0 * n_clad) / k0)
+
+
+# visualize k**2(x)
+
+plt.figure(figsize=(10,6))
+
+plt.plot(
+    x * 1e6,
+    k_squared,
+)
+
+plt.xlabel("x (µm)")
+plt.ylabel(r"$k_0^2 n^2(x)$")
+plt.title(r"Optical potential term $k_0^2 n^2(x)$")
+plt.grid()
+plt.show()
+
+# plot
+plt.plot(
+    x * 1e6,
+    n,
+    label=f"N = {N}"
+)
 
 
 
